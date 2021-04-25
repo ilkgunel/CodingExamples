8c6a09ee1894711e4bb3ec3ea7a4e1ec698f9ecf
İlkay Günel <ilkay.gunel@kod5.org>
2021-04-25 17:36:30 +0300

    Adding FairIndexSolution

---
+++ CodingExamples/src/main/java/com/ilkaygunel/coding/FairIndexSolution.java
@@ -0,0 +1,53 @@
+package com.ilkaygunel.coding;
+
+/**
+ * @author ilkay.gunel
+ */
+
+import java.util.*;
+
+public class FairIndexSolution {
+    public static void main(String[] args) {
+        Map<Integer, Integer> sumForFairIndexesForFirstArray = new HashMap<>();
+        Map<Integer, Integer> sumForFairIndexesForSecondArray = new HashMap<>();
+
+        int[] firstArray = {4, -1, 0, 3};
+        int[] secondArray = {-2, 6, 0, 4};
+
+        FairIndexSolution fairIndexSolution = new FairIndexSolution();
+        fairIndexSolution.findFairIndexAndSum(firstArray, sumForFairIndexesForFirstArray);
+        fairIndexSolution.findFairIndexAndSum(secondArray, sumForFairIndexesForSecondArray);
+
+        sumForFairIndexesForFirstArray.entrySet().forEach(entry -> {
+            sumForFairIndexesForSecondArray.entrySet().removeIf(item ->
+                    (entry.getKey() == item.getKey() && entry.getValue() != item.getValue()) ||
+                            !sumForFairIndexesForFirstArray.containsKey(item.getKey()) ||
+                            !sumForFairIndexesForSecondArray.containsKey(entry.getKey())
+            );
+        });
+
+        System.out.println("Fair Indexes:");
+        sumForFairIndexesForSecondArray.entrySet().forEach(item -> {
+            System.out.println((item.getKey() + 1) + ":" + item.getValue());
+        });
+        System.out.println("Output:" + sumForFairIndexesForSecondArray.size());
+    }
+
+    private void findFairIndexAndSum(int[] arrayForProcessing, Map<Integer, Integer> mapForResult) {
+        for (int i = 0; i < arrayForProcessing.length; i++) {
+            int sum1 = 0;
+            int sum2 = 0;
+            int[] subArray1 = Arrays.copyOfRange(arrayForProcessing, 0, i + 1);
+            for (int k = 0; k < subArray1.length; k++) {
+                sum1 = sum1 + subArray1[k];
+            }
+            int[] subArray2 = Arrays.copyOfRange(arrayForProcessing, i + 1, arrayForProcessing.length);
+            for (int j = 0; j < subArray2.length; j++) {
+                sum2 = sum2 + subArray2[j];
+            }
+            if ((sum1 == sum2) && subArray1.length > 0 && subArray2.length > 0) {
+                mapForResult.put(i, sum1);
+            }
+        }
+    }
+}
